a42c95f9a6077ea1d4bb14537ca1083e909ccdc8
xudusheng <597132100@qq.com>
2017-03-26 07:55:31 +0800

    Initial Commit of XDSModuleManager
    Signed-off-by: xudusheng <597132100@qq.com>

---
+++ Example/Pods/Target Support Files/Pods-XDSModuleManager_Example/Pods-XDSModuleManager_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XDSModuleManager_Example : NSObject
+@end
+@implementation PodsDummy_Pods_XDSModuleManager_Example
+@end
---
+++ Example/Pods/Target Support Files/Pods-XDSModuleManager_Tests/Pods-XDSModuleManager_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_XDSModuleManager_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_XDSModuleManager_Tests
+@end
